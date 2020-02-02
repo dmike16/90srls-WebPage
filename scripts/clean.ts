70f3ffaddb27ee7fc35e7cd25dbb8b75656753f0
dmike16 <cipmiky@gmail.com>
2020-02-02 21:12:03 +0100

    build: change to sublime text

--- scripts/clean.ts
+++ scripts/clean.ts
@@ -3,8 +3,8 @@ import pkg from "../lib/package";
 
 const del = require('del');
 
-export default async function(args: any, log: Logger){
-    log.info("Cleaning project");
-    await del(pkg.buildCtx.output.parent);
-    return 0;
+export default async function(args: any, log: Logger) {
+  log.info("Cleaning project");
+  await del(pkg.buildCtx.output.parent);
+  return 0;
 }
