b23d0b651aa0515b0b785102083f6a2496ff8ca1
dmike <cipmiky@gmail.com>
2020-12-12 16:22:31 +0100

    build: add vuejs config to build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,31 +1,31 @@
-import { Logger } from "../tools/logger";
-import { WebpackCompilationOutput, webpackBundle } from "../tools/webpack/compiler";
-import common from "../tools/webpack/create-config/common";
-import assets from "../tools/webpack/create-config/assets";
-import styles from "../tools/webpack/create-config/styles";
-import broswer from "../tools/webpack/create-config/broswer"
+import vuejs from "../tools/webpack/create-config/vuejs";
 import pkg from "../lib/package";
-import babel from "../tools/webpack/create-config/babel";
+import {Logger} from "../tools/logger";
+import {webpackBundle, WebpackCompilationOutput} from "../tools/webpack/compiler";
+import assets from "../tools/webpack/create-config/assets";
+import broswer from "../tools/webpack/create-config/broswer";
+import common from "../tools/webpack/create-config/common";
+import styles from "../tools/webpack/create-config/styles";
+import {merge} from "webpack-merge";
 
-const merge = require('webpack-merge');
 
 export default async function (args: any, log: Logger) {
-    const mode = "production";
-    pkg.buildCtx.extractCss = true;
-    const configuratiion = [
-        common(mode)
-        , assets(mode)
-        , styles(mode)
-        , broswer(mode)
-        , babel(mode)
-    ];
-    try {
-        const bundle: WebpackCompilationOutput = await webpackBundle(merge(configuratiion));
-        log.info(bundle.result);
-    } catch (err) {
-        log.error(err.error || err);
-        return 1;
-    }
+  const mode = "production";
+  pkg.buildCtx.extractCss = true;
+  const configuratiion = [
+    common(mode)
+    , vuejs(mode)
+    , assets(mode)
+    , styles(mode)
+    , broswer(mode)
+  ];
+  try {
+    const bundle: WebpackCompilationOutput = await webpackBundle(merge(configuratiion));
+    log.info(bundle.result);
+  } catch (err) {
+    log.error(err.error || err);
+    return 1;
+  }
 
-    return 0;
+  return 0;
 }
