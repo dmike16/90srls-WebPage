5aad98ec7764cbcb34f0219c43a765bce8eb7db6
dmike <cipmiky@gmail.com>
2021-01-17 23:33:47 +0100

    build: add pretttier sdk + eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2018,
     sourceType: 'module',
   },
   extends: [
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended'
   ],
   rules: {
     // place to specify ESLint rules - can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "vue/no-multiple-template-root": "off"
   }
 };
